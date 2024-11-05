1b3ea987ed50a265460742c87010945f90ec51b6
Hungnth <thienhungnth@gmail.com>
2024-11-05 15:03:33 +0700

    Update

---
+++ directadmin-permisson.sh
@@ -0,0 +1,165 @@
+chmod 700 /usr/local/directadmin/custombuild/
+chmod 711 /usr/local/directadmin/data/
+chmod 711 /usr/local/directadmin/plugins/
+chmod 711 /usr/local/directadmin/scripts/
+chmod 600 /usr/local/directadmin/custombuild.tar.gz
+chmod 700 /usr/local/directadmin/da-popb4smtp
+chmod 700 /usr/local/directadmin/directadmin
+
+chmod 755 /usr/local/directadmin/custombuild/configure/ap2/configure.apache
+chmod 755 /usr/local/directadmin/custombuild/configure/ap2/configure.modsecurity
+chmod 755 /usr/local/directadmin/custombuild/configure/ap2/configure.php53
+chmod 755 /usr/local/directadmin/custombuild/configure/ap2/configure.php54
+chmod 755 /usr/local/directadmin/custombuild/configure/ap2/configure.php55
+chmod 755 /usr/local/directadmin/custombuild/configure/ap2/configure.php56
+chmod 755 /usr/local/directadmin/custombuild/configure/ap2/configure.php70
+chmod 755 /usr/local/directadmin/custombuild/configure/ap2/configure.php71
+chmod 755 /usr/local/directadmin/custombuild/configure/ap2/configure.php72
+chmod 755 /usr/local/directadmin/custombuild/configure/ap2/configure.php73
+chmod 755 /usr/local/directadmin/custombuild/configure/ap2/configure.php74
+chmod 755 /usr/local/directadmin/custombuild/configure/clamav/configure.clamav
+chmod 755 /usr/local/directadmin/custombuild/configure/curl/configure.curl
+chmod 755 /usr/local/directadmin/custombuild/configure/imagemagick/configure.imagemagick
+chmod 755 /usr/local/directadmin/custombuild/configure/libxml2/configure.libxml2
+chmod 755 /usr/local/directadmin/custombuild/configure/libxslt/configure.libxslt
+chmod 755 /usr/local/directadmin/custombuild/configure/modsecurity/configure.modsecurity
+
+chmod 755 /usr/local/directadmin/custombuild/configure/nginx/configure.modsecurity
+chmod 755 /usr/local/directadmin/custombuild/configure/nginx/configure.nginx
+
+chmod 755 /usr/local/directadmin/custombuild/configure/nginx_reverse/configure.modsecurity
+chmod 755 /usr/local/directadmin/custombuild/configure/nginx_reverse/configure.nginx
+chmod 755 /usr/local/directadmin/custombuild/configure/openlitespeed/configure.openlitespeed
+
+
+chmod 755 /usr/local/directadmin/custombuild/configure/fastcgi/fcgid.sh
+chmod 755 /usr/local/directadmin/custombuild/configure/fastcgi/fcgid_freebsd.sh
+
+chmod 755 /usr/local/directadmin/custombuild/configure/php/configure.php53
+chmod 755 /usr/local/directadmin/custombuild/configure/php/configure.php54
+chmod 755 /usr/local/directadmin/custombuild/configure/php/configure.php55
+chmod 755 /usr/local/directadmin/custombuild/configure/php/configure.php56
+chmod 755 /usr/local/directadmin/custombuild/configure/php/configure.php70
+chmod 755 /usr/local/directadmin/custombuild/configure/php/configure.php71
+chmod 755 /usr/local/directadmin/custombuild/configure/php/configure.php72
+chmod 755 /usr/local/directadmin/custombuild/configure/php/configure.php73
+chmod 755 /usr/local/directadmin/custombuild/configure/php/configure.php74
+chmod 755 /usr/local/directadmin/custombuild/configure/php/configure.php80
+chmod 755 /usr/local/directadmin/custombuild/configure/php/configure.php81
+chmod 755 /usr/local/directadmin/custombuild/configure/php/configure.php82
+
+chmod 755 /usr/local/directadmin/custombuild/configure/phpmyadmin/config.inc.php
+chmod 755 /usr/local/directadmin/custombuild/configure/proftpd/configure.proftpd
+chmod 755 /usr/local/directadmin/custombuild/configure/pureftpd/configure.pureftpd
+chmod 755 /usr/local/directadmin/custombuild/configure/pureftpd/pureftpd_sni.sh
+chmod 755 /usr/local/directadmin/custombuild/configure/pureftpd/pureftpd_uploadscan.sh
+chmod 755 /usr/local/directadmin/custombuild/configure/suhosin/php_uploadscan.sh
+chmod 755 /usr/local/directadmin/custombuild/configure/suphp/configure.suphp
+
+chmod 755 /usr/local/directadmin/custombuild/configure/unit/configure.unit
+chmod 755 /usr/local/directadmin/custombuild/configure/unit/unit_checks.sh
+
+chmod 755 /usr/local/directadmin/custombuild/redis-5.3.7/config.nice
+chmod 755 /usr/local/directadmin/custombuild/redis-5.3.7/config.status
+chmod 755 /usr/local/directadmin/custombuild/redis-5.3.7/configure
+chmod 755 /usr/local/directadmin/custombuild/redis-5.3.7/libtool
+
+chmod 755 /usr/local/directadmin/custombuild/redis-5.3.7/build/config.guess
+chmod 755 /usr/local/directadmin/custombuild/redis-5.3.7/build/config.sub
+chmod 755 /usr/local/directadmin/custombuild/redis-5.3.7/build/shtool
+
+chmod 755 /usr/local/directadmin/custombuild/redis-5.3.7/tests/make-cluster.sh
+chmod 755 /usr/local/directadmin/custombuild/redis-5.3.7/tests/mkring.sh
+chmod 755 /usr/local/directadmin/data/skins/packskins.sh
+
+chmod 755 /usr/local/directadmin/data/skins/enhanced/CHANGELOG
+chmod 755 /usr/local/directadmin/data/skins/enhanced/confirmation.html
+chmod 755 /usr/local/directadmin/data/skins/enhanced/creation_check.js
+chmod 755 /usr/local/directadmin/data/skins/enhanced/files_admin.conf
+chmod 755 /usr/local/directadmin/data/skins/enhanced/files_reseller.conf
+chmod 755 /usr/local/directadmin/data/skins/enhanced/files_user.conf
+chmod 755 /usr/local/directadmin/data/skins/enhanced/footer.html
+chmod 755 /usr/local/directadmin/data/skins/enhanced/footer_bar_1.html
+chmod 755 /usr/local/directadmin/data/skins/enhanced/footer_bar_2.html
+chmod 755 /usr/local/directadmin/data/skins/enhanced/footer_full.html
+chmod 755 /usr/local/directadmin/data/skins/enhanced/footer_wide.html
+chmod 755 /usr/local/directadmin/data/skins/enhanced/header.html
+chmod 755 /usr/local/directadmin/data/skins/enhanced/header_bar.html
+chmod 755 /usr/local/directadmin/data/skins/enhanced/header_full.html
+chmod 755 /usr/local/directadmin/data/skins/enhanced/header_wide.html
+chmod 755 /usr/local/directadmin/data/skins/enhanced/javascript.html
+chmod 755 /usr/local/directadmin/data/skins/enhanced/livesearch.js
+chmod 755 /usr/local/directadmin/data/skins/enhanced/loggedout.html
+chmod 755 /usr/local/directadmin/data/skins/enhanced/nothing.html
+chmod 755 /usr/local/directadmin/data/skins/enhanced/passwd.html
+chmod 755 /usr/local/directadmin/data/skins/enhanced/set_permissions.sh
+chmod 755 /usr/local/directadmin/data/skins/enhanced/skin.conf
+chmod 755 /usr/local/directadmin/data/skins/enhanced/style.css
+chmod 755 /usr/local/directadmin/data/skins/enhanced/style_old.css
+chmod 755 /usr/local/directadmin/data/skins/enhanced/template.html
+chmod 755 /usr/local/directadmin/data/skins/enhanced/visibility.js
+chmod 755 /usr/local/directadmin/data/templates/default/index.html
+
+chmod -R 755 /usr/local/directadmin/data/skins/enhanced/admin/*
+chmod -R 755 /usr/local/directadmin/data/skins/enhanced/images/*
+chmod -R 755 /usr/local/directadmin/data/skins/enhanced/lang/*
+chmod -R 755 /usr/local/directadmin/data/skins/enhanced/reseller/*
+chmod -R 755 /usr/local/directadmin/data/skins/enhanced/user/*
+chmod -R 755 /usr/local/directadmin/data/skins/evolution/*
+chmod -R 755 /usr/local/directadmin/data/skins/power_user/*
+chmod -R 755 /usr/local/directadmin/plugins/csf/admin/*
+chmod -R 755 /usr/local/directadmin/plugins/csf/exec/*
+chmod -R 755 /usr/local/directadmin/plugins/csf/reseller/*
+chmod -R 755 /usr/local/directadmin/plugins/csf/scripts/*
+chmod -R 755 /usr/local/directadmin/plugins/custombuild/admin/*
+
+chmod 711 /usr/local/directadmin/data/templates/custom/
+chmod 711 /usr/local/directadmin/data/templates/email_pass_change/
+chmod 711 /usr/local/directadmin/data/templates/feature_sets/
+chmod 711 /usr/local/directadmin/data/templates/ftp_pass_change/
+chmod 711 /usr/local/directadmin/data/templates/mx/
+chmod 711 /usr/local/directadmin/data/templates/sharedip/
+chmod 711 /usr/local/directadmin/data/templates/suspended/
+chmod 711 /usr/local/directadmin/data/templates/userdir/
+
+chmod 711 /usr/local/directadmin/data/templates/feature_sets/core_functions/
+chmod 711 /usr/local/directadmin/data/templates/feature_sets/dns_only/
+chmod 711 /usr/local/directadmin/data/templates/feature_sets/email_only/
+
+chmod 700 /usr/local/directadmin/data/users/admin/login_keys/rootauth/
+chmod 600 /usr/local/directadmin/data/users/admin/login_keys/rootauth/key.conf
+chmod 511 /usr/local/directadmin/data/users/admin/php/fcgid/
+
+chmod 700 /usr/local/directadmin/plugins/custombuild/configure/
+chmod 700 /usr/local/directadmin/plugins/custombuild/logs/
+chmod 700 /usr/local/directadmin/plugins/custombuild/scripts/
+
+chmod 4755 /usr/local/directadmin/plugins/custombuild/admin/build
+
+chmod 700 /usr/local/directadmin/plugins/custombuild/configure/ap2/
+chmod 700 /usr/local/directadmin/plugins/custombuild/configure/clamav/
+chmod 700 /usr/local/directadmin/plugins/custombuild/configure/dovecot/
+chmod 700 /usr/local/directadmin/plugins/custombuild/configure/libxml2/
+chmod 700 /usr/local/directadmin/plugins/custombuild/configure/libxslt/
+chmod 700 /usr/local/directadmin/plugins/custombuild/configure/litespeed/
+chmod 700 /usr/local/directadmin/plugins/custombuild/configure/php/
+chmod 700 /usr/local/directadmin/plugins/custombuild/configure/proftpd/
+chmod 700 /usr/local/directadmin/plugins/custombuild/configure/pureftpd/
+
+chmod 700 /usr/local/directadmin/plugins/custombuild/scripts/install.sh
+chmod 700 /usr/local/directadmin/plugins/custombuild/scripts/uninstall.sh
+chmod 700 /usr/local/directadmin/plugins/custombuild/scripts/update.sh
+
+chmod 700 /usr/local/directadmin/scripts/custom/
+chmod 700 /usr/local/directadmin/scripts/packages/
+
+chmod 700 -R /usr/local/directadmin/scripts/*
+
+chmod 755 /usr/local/directadmin/scripts/decrypt_file.sh
+chmod 755 /usr/local/directadmin/scripts/encrypt_file.sh
+chmod 755 /usr/local/directadmin/scripts/redirect.php
+chmod 644 /usr/local/directadmin/scripts/php_clean.ini
+
+
+
+
