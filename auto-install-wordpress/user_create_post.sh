c165bb71d5c163045749ffd414e44ec7f8290b5d
Nguyen Thien Hung <63095696+Hungnth@users.noreply.github.com>
2024-11-09 15:53:03 +0700

    Update user_create_post.sh

--- auto-install-wordpress/user_create_post.sh
+++ auto-install-wordpress/user_create_post.sh
@@ -60,42 +60,24 @@ su -s /bin/bash -c "${WP_CLI_PATH} theme delete twentytwentythree" $username
 su -s /bin/bash -c "${WP_CLI_PATH} plugin delete hello" $username
 su -s /bin/bash -c "${WP_CLI_PATH} plugin delete akismet" $username
 
-# Install Plugins, add flag '--active' to active plugin after install
-echo "Install plugin Admin And Site Enhancements Pro (ASE)"
-su -s /bin/bash -c "${WP_CLI_PATH} plugin install 'https://d3cav5r4mkyokm.cloudfront.net/staging/c9a7aebb-5ab3-41de-8e76-a5685f399a81/660230e0cffab0005b80c518/A-ME-2024-VL0G-1730337071199.zip'" $username # Admin And Site Enhancements Pro (ASE)
-echo "Install plugin Advanced Custom Fields Pro (ACF)"
-su -s /bin/bash -c "${WP_CLI_PATH} plugin install 'https://d3cav5r4mkyokm.cloudfront.net/staging/c9a7aebb-5ab3-41de-8e76-a5685f399a81/660230e0cffab0005b80c518/A-ME-2024-GBEE-1730683123443.zip'" $username # Advanced Custom Fields Pro (ACF)
-echo "Install plugin Rank Math SEO Free"
-su -s /bin/bash -c "${WP_CLI_PATH} plugin install seo-by-rank-math" $username # Rank Math SEO
-echo "Install plugin Rank Math SEO Pro"
-su -s /bin/bash -c "${WP_CLI_PATH} plugin install 'https://d3cav5r4mkyokm.cloudfront.net/staging/c9a7aebb-5ab3-41de-8e76-a5685f399a81/660230e0cffab0005b80c518/A-ME-2024-419T-1729215448856.zip'" $username # Rank Math SEO Pro
-echo "Install plugin WP Rocket"
-su -s /bin/bash -c "${WP_CLI_PATH} plugin install 'https://d3cav5r4mkyokm.cloudfront.net/staging/c9a7aebb-5ab3-41de-8e76-a5685f399a81/660230e0cffab0005b80c518/A-ME-2024-9YO0-1730687479377.zip'" $username # WP Rocket
-su -s /bin/bash -c "${WP_CLI_PATH} plugin install contact-form-7" $username # Contact Form 7
-
-# Install Theme, add flag '--active' to active plugin after install
-echo "Install theme Flatsome"
-su -s /bin/bash -c "${WP_CLI_PATH} theme install 'https://d3cav5r4mkyokm.cloudfront.net/staging/c9a7aebb-5ab3-41de-8e76-a5685f399a81/660230e0cffab0005b80c518/A-ME-2024-V8QO-1728457093405.zip' --activate" $username # Flatsome
-su -s /bin/bash -c "${WP_CLI_PATH} theme delete twentytwentyfour" $username #
-
 # Options
-su -s /bin/bash -c "${WP_CLI_PATH} language core install vi_VN" $username
-su -s /bin/bash -c "${WP_CLI_PATH} site switch-language vi_VN" $username
-su -s /bin/bash -c "${WP_CLI_PATH} option update timezone_string 'Asia/Ho_Chi_Minh'" $username
-su -s /bin/bash -c "${WP_CLI_PATH} option update time_format 'H:i'" $username
-su -s /bin/bash -c "${WP_CLI_PATH} option update date_format 'd/m/Y'" $username
-su -s /bin/bash -c "${WP_CLI_PATH} option update large_size_w 0" $username
-su -s /bin/bash -c "${WP_CLI_PATH} option update large_size_h 0" $username
-su -s /bin/bash -c "${WP_CLI_PATH} option update medium_large_size_w 0" $username
-su -s /bin/bash -c "${WP_CLI_PATH} option update medium_large_size_h 0" $username
-su -s /bin/bash -c "${WP_CLI_PATH} option update medium_size_w 0" $username
-su -s /bin/bash -c "${WP_CLI_PATH} option update medium_size_h 0" $username
-su -s /bin/bash -c "${WP_CLI_PATH} option update thumbnail_size_w 0" $username
-su -s /bin/bash -c "${WP_CLI_PATH} option update thumbnail_size_h 0" $username
-su -s /bin/bash -c "${WP_CLI_PATH} option update thumbnail_crop 0" $username
-su -s /bin/bash -c "${WP_CLI_PATH} option update comment_moderation 1" $username
-su -s /bin/bash -c "${WP_CLI_PATH} option update default_pingback_flag 0" $username
-su -s /bin/bash -c "${WP_CLI_PATH} option update default_ping_status closed" $username
+# su -s /bin/bash -c "${WP_CLI_PATH} language core install vi_VN" $username
+# su -s /bin/bash -c "${WP_CLI_PATH} site switch-language vi_VN" $username
+# su -s /bin/bash -c "${WP_CLI_PATH} option update timezone_string 'Asia/Ho_Chi_Minh'" $username
+# su -s /bin/bash -c "${WP_CLI_PATH} option update time_format 'H:i'" $username
+# su -s /bin/bash -c "${WP_CLI_PATH} option update date_format 'd/m/Y'" $username
+# su -s /bin/bash -c "${WP_CLI_PATH} option update large_size_w 0" $username
+# su -s /bin/bash -c "${WP_CLI_PATH} option update large_size_h 0" $username
+# su -s /bin/bash -c "${WP_CLI_PATH} option update medium_large_size_w 0" $username
+# su -s /bin/bash -c "${WP_CLI_PATH} option update medium_large_size_h 0" $username
+# su -s /bin/bash -c "${WP_CLI_PATH} option update medium_size_w 0" $username
+# su -s /bin/bash -c "${WP_CLI_PATH} option update medium_size_h 0" $username
+# su -s /bin/bash -c "${WP_CLI_PATH} option update thumbnail_size_w 0" $username
+# su -s /bin/bash -c "${WP_CLI_PATH} option update thumbnail_size_h 0" $username
+# su -s /bin/bash -c "${WP_CLI_PATH} option update thumbnail_crop 0" $username
+# su -s /bin/bash -c "${WP_CLI_PATH} option update comment_moderation 1" $username
+# su -s /bin/bash -c "${WP_CLI_PATH} option update default_pingback_flag 0" $username
+# su -s /bin/bash -c "${WP_CLI_PATH} option update default_ping_status closed" $username
 
 # Create .htaccess
 cat << EOF > /home/$username/domains/$domain/public_html/.htaccess
